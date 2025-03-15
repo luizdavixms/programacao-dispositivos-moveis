0f9ee4157ab9ccb4e1a0e5eac8af154e1883aa77
unknown <2314290098@ACADEMICO.IESB.BR>
2025-03-14 21:21:06 -0300

    adicionando a pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,18 @@
+import GalleryScreen from "./screens/GalleryScreen"
+import HomeScreen from "./screens/HomeScreen"
+import ListScreen from "./screens/ListScreen"
+import ProfileScreen from "./screens/ProfileScreen"
+
+
+function App() {
+  return(
+    <>
+      {/* <ListScreen/> */}
+      {/* <GalleryScreen/> */}
+      {/* <HomeScreen/> */}
+      <ProfileScreen/>
+    </>
+  )
+}
+
+export default App
