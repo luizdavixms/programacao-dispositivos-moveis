71a2bef9784fe0b18823ca507a27e4d2ed2c4075
unknown <2314290098@ACADEMICO.IESB.BR>
2025-03-31 20:28:28 -0300

    Adicionando pratica05

---
+++ praticas/pratica05/App.js
@@ -0,0 +1,19 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./screens/LoginScreen";
+// import ChatScreen from "./screens/ChatScreen";
+// import ProfileScreen from "./screens/ProfileScreen";
+// import SettingsScreen from "./screens/SettingsScreen";
+
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <LoginScreen />
+      {/* <ChatScreen />
+      <ProfileScreen />
+      <SettingsScreen /> */}
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
